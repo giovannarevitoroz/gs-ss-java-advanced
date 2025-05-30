3c7f9aa9e5299cfef4d48a859ca8cfe86f7f24e2
Giovanna <85405443+giovannarevitoroz@users.noreply.github.com>
2025-05-30 20:31:00 -0300

    Delete src directory

--- src/main/java/com/fiap/gs_swift_safety/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fiap.gs_swift_safety.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
